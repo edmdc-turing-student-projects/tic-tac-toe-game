b71639e014dbb9f3b337075c2cf3d90aba4084e9
emontealvo <edwin.montealvo@gmail.com>
2020-04-21 19:12:31 -0600

    Add finishing touches alphabetize css rules add missing semicolons in end of statements change some variable names to be clearer

--- src/game-board.js
+++ src/game-board.js
@@ -3,7 +3,7 @@ var player2 = new Player ({id:'player2', token:'o'});
 
 class GameBoard {
   constructor() {
-    this.gameBoard = [
+    this.game = [
       [null, null, null],
       [null, null, null],
       [null, null, null]
@@ -11,12 +11,12 @@ class GameBoard {
     this.turnLoop = 0;
     this.hadDraw = false;
     this.hadVictory = false;
-  }
+  };
 
   startGame(player1, player2) {
     player1.turn = true;
     this.startTurn(player1, player2);
-  }
+  };
   
   delegateTurn(player1, player2) {
     (player1.turn === true) ? this.startTurn(player1, player2) :
@@ -29,13 +29,13 @@ class GameBoard {
     this.checkForWins(player);
     this.checkForDraw();
     this.endTurn(player, otherPlayer);
-  }
+  };
 
   placePlayerToken(player) {
     let row = player.tokenPlacement.row;
     let column = player.tokenPlacement.column;
-    if (this.gameBoard[row][column] === null) {
-      this.gameBoard[row][column] = player.token;
+    if (this.game[row][column] === null) {
+      this.game[row][column] = player.token;
     };
   };
 
@@ -47,85 +47,84 @@ class GameBoard {
 
   accessAllRows(player) {
     for (let i = 0; i < 3; i++) {
-      let row = this.gameBoard[i];
-      this.checkRows(player, row)
+      let row = this.game[i];
+      this.checkRow(player, row);
     };
-  }
+  };
 
-  checkRows(player, row) {
+  checkRow(player, row) {
     let rowCheck = row.filter(function(tokenSpot) {
       return tokenSpot === player.token});
     if (rowCheck.length === 3) {
-      this.claimWin(player) 
+      this.claimWin(player);
     };
-  } 
+  }; 
   
   accessAllColumns(player) {
-    let columnIndexNumbers = [0, 1, 2];
-    columnIndexNumbers.forEach( columnIndex => 
-      this.getSingleColumn(player, columnIndex));
+    let columnSequence = [0, 1, 2];
+    columnSequence.forEach( columnNumber => 
+      this.getSingleColumn(player, columnNumber));
   };
 
   getSingleColumn(player, columnNumber) {
-    let column = []
+    let column = [];
     for (let i = 0; i < 3; i++) {
-      column.push(`${this.gameBoard[i][columnNumber]}`)
+      column.push(`${this.game[i][columnNumber]}`)
     };
-    this.checkColumn(player, column)
+    this.checkColumn(player, column);
   };
 
   checkColumn(player, column) {
-    let filteredColumns = column.filter(function(rowToken) {
-      return rowToken === player.token});
+    let filteredColumns = column.filter(function(tokenSpot) {
+      return tokenSpot === player.token});
     if (filteredColumns.length === 3) {
-      this.claimWin(player) 
+      this.claimWin(player); 
     };
   };
   
   checkCenterPiece(player) {
-    if (this.gameBoard[1][1] === player.token) {
+    if (this.game[1][1] === player.token) {
       this.checkDiagonals(player);
     };
   };
 
   checkDiagonals(player) {
-    if (this.gameBoard[1][1] === this.gameBoard[0][2] 
-      && this.gameBoard[1][1] === this.gameBoard[2][0]) {
+    if (this.game[1][1] === this.game[0][2] 
+      && this.game[1][1] === this.game[2][0]) {
         this.claimWin(player);
-    } else if (this.gameBoard[1][1] === this.gameBoard[2][2] 
-      && this.gameBoard[1][1] === this.gameBoard[0][0]) {
-        this.claimWin(player) 
-    }
+    } else if (this.game[1][1] === this.game[2][2] 
+      && this.game[1][1] === this.game[0][0]) {
+        this.claimWin(player); 
+    };
   };
 
   checkForDraw() {
    if (this.turnLoop === 8 && this.hadVictory === false) {
      this.hadDraw = true;
-     alert("It's a Draw :(")
-   }
-  }
+   };
+  };
   
   endTurn(player, otherPlayer) {
-    this.turnLoop++
+    this.turnLoop++;
     player.turn = false;
     otherPlayer.turn = true;
   };
 
   claimWin(player) {
-    player.wins.push(this.gameBoard);
+    player.wins.push(this.game);
     player.isWinner = true;
     player.saveWinsToStorage();
     this.hadVictory = true;
   };
 
   endGame() {
-    this.gameBoard = [
+    this.game = [
       [null, null, null],
       [null, null, null],
       [null, null, null]
-    ]
+    ];
     this.turnLoop = 0;
     this.hadDraw = false;
     this.hadVictory = false;
-  }
+  };
 };
--- src/main.js
+++ src/main.js
@@ -9,16 +9,16 @@ window.onload = checkForPlayers();
 
 playerInputForm.addEventListener('keyup', enableStartButton);
 playerInputForm.addEventListener('submit', submitPlayerNames);
-gameContainer.addEventListener('click', determineTokenPlacement)
-reset.addEventListener('click', resetVictoryCount)
+gameContainer.addEventListener('click', startTurnLoop);
+reset.addEventListener('click', resetVictoryCount);
 
 function checkForPlayers() {
-  let retrivedPlayers = JSON.parse(localStorage.getItem('players'))
+  let retrivedPlayers = JSON.parse(localStorage.getItem('players'));
   renamePlayers(retrivedPlayers);
   if (player1.name !== "" && player2.name !== "") {
     displayFirstGame();
-  }
-}
+  };
+};
 
 function renamePlayers(playerList) {
   if (playerList) {
@@ -27,95 +27,95 @@ function renamePlayers(playerList) {
   } else {
     player1.name = "";
     player2.name = "";
-  }
-}
+  };
+};
 
 function enableStartButton() {
   if (playerInputForm[0].value !== '' && playerInputForm[1].value !== '') {
     playerInputForm[2].disabled = false;
- }
-}
+  };
+};
 
-function submitPlayerNames() {
+function submitPlayerNames(event) {
+  event.preventDefault();
   player1.name = playerInputForm[0].value; 
   player2.name = playerInputForm[1].value;
   savePlayersToStorage();
   displayFirstGame();
-}
+};
 
 function savePlayersToStorage() {
   let players = [player1.name, player2.name];
-  localStorage.setItem('players', JSON.stringify(players))
-}
+  localStorage.setItem('players', JSON.stringify(players));
+};
 
 function displayFirstGame() {
   assignNamesToPlayerColumn();
   displayGameBoard();
   displayPreviousWins();
   displayPlayerTurn();
-}
+};
 
 function assignNamesToPlayerColumn() {
-  let playerTitles = document.querySelectorAll('.player-info')
+  let playerTitles = document.querySelectorAll('.player-info');
   playerTitles[0].children[0].innerText = player1.name;
   playerTitles[1].children[0].innerText = player2.name;
-}
+};
 
 function displayGameBoard() {
   playerInputForm.classList.add('hidden');
   gameContainer.children[2].classList.remove('hidden');
   reset.classList.remove('hidden');
-}
+};
 
 function displayPreviousWins() {
   player1.retrieveWinsFromStorage();
-  player2.retrieveWinsFromStorage();
   renderRetrievedWins(player1);
+  player2.retrieveWinsFromStorage();
   renderRetrievedWins(player2);
-}
+};
 
 function renderRetrievedWins(player) {
   for (let i = 0; i < player.wins.length; i++) {
-    let gameGrid = player.wins[i]
-    attachMiniBoardToPlayer(player, gameGrid)
-  }
-}
-
-function determineTokenPlacement(event) {
-  if (event.target.innerText === '' && event.path[1].classList.contains('game-board')) {
-    let translatedLocation = getLocation(event);
-    attachTokenToPlayer(translatedLocation, player1, player2)
-    renderTokenPlacement(event, player1, player2)
-    gameBoard.delegateTurn(player1, player2);
-  }
-  checkResults();
-}
-
-function getLocation (event) {
-  let chosenGameBoardBox = event.target.closest('div')
-  let gameBoardLocation = {
-    row: Number.parseInt(chosenGameBoardBox.dataset.row), 
-    column: Number.parseInt(chosenGameBoardBox.dataset.column)
+    let gameGrid = player.wins[i];
+    attachMiniBoardToPlayer(player, gameGrid);
   };
-  return gameBoardLocation
-}
+};
+
+function startTurnLoop(event) {
+  if (event.target.innerText === '' && event.target.parentElement.classList.contains('game-board')) {
+    translateLocationToPlayer(event);
+    renderTokenPlacement(event, player1, player2);
+    gameBoard.delegateTurn(player1, player2);
+    checkResults();
+  };
+};
+
+function translateLocationToPlayer (event) {
+  let chosenGameBox = event.target.closest('div');
+  let translatedLocation = {
+    row: Number.parseInt(chosenGameBox.dataset.row), 
+    column: Number.parseInt(chosenGameBox.dataset.column)
+  };
+  attachTokenToPlayer(translatedLocation, player1, player2);
+};
 
 function attachTokenToPlayer(translatedLocation, player1, player2) {
   (player1.turn === true) ? player1.tokenPlacement = translatedLocation :
   (player2.turn === true) ? player2.tokenPlacement = translatedLocation :
-    player1.tokenPlacement = translatedLocation
-}
+    player1.tokenPlacement = translatedLocation;
+};
 
 function renderTokenPlacement(event, player1, player2) {
   (player1.turn === true) ? renderToken(event, player1) :
   (player2.turn === true) ? renderToken(event, player2) :
     renderToken(event, player1);  
-}
+};
 
 function renderToken(event, player) {
   let chosenGameBoardBox = event.target.closest('div');
   chosenGameBoardBox.innerText = player.token;
-}
+};
 
 function checkResults() {
   if (gameBoard.hadDraw) {
@@ -123,25 +123,25 @@ function checkResults() {
   } else if (gameBoard.hadVictory) {
     displayResults();
     checkPlayersForWin(player1, player2);
-  } else if (gameBoard.hadDraw === false && gameBoard.hadVictory === false) {
+  } else {
     displayPlayerTurn();
-  }
-}
+  };
+};
 
 function displayResults () {
   if (player1.isWinner) {
-    turnDisplay.innerHTML = `<h3>${player1.name} Won!</h3>`
+    turnDisplay.innerHTML = `<h3>${player1.name} Won!</h3>`;
   } else if (player2.isWinner) {
-    turnDisplay.innerHTML = `<h3>${player2.name} Won!</h3>`
+    turnDisplay.innerHTML = `<h3>${player2.name} Won!</h3>`;
   } else if (gameBoard.hadDraw) {
-    turnDisplay.innerHTML = `<h3>It'a a Draw!</h3>`
-  }
-  setTimeout(resetGameBoard, 3000)
-  setTimeout(displayPlayerTurn, 3000)
-}
+    turnDisplay.innerHTML = `<h3>It'a a Draw!</h3>`;
+  };
+  setTimeout(resetGameBoard, 2000);
+  setTimeout(displayPlayerTurn, 2000);
+};
   
 function checkPlayersForWin(player1, player2) {
-  let gameGrid = gameBoard.gameBoard;
+  let gameGrid = gameBoard.game;
   if (player1.isWinner) {
     attachMiniBoardToPlayer(player1, gameGrid);
     player1.isWinner = false;
@@ -149,64 +149,60 @@ function checkPlayersForWin(player1, player2) {
     attachMiniBoardToPlayer(player2, gameGrid);
     player2.isWinner = false;
   };
-}
+};
 
 function attachMiniBoardToPlayer(player, gameGrid) {
-  let winningPlayerStats = document.getElementById(player.id)
-  let playerVictoryColumn = winningPlayerStats.querySelector('.player-victories')
-  createMiniBoard(playerVictoryColumn, gameGrid)
+  let winningPlayerStats = document.getElementById(player.id);
+  let playerVictoryColumn = winningPlayerStats.querySelector('.player-victories');
+  createMiniBoard(playerVictoryColumn, gameGrid);
   updatePlayerWinCount(player, winningPlayerStats);
-}
+};
 
 function createMiniBoard(playerVictoryColumn, gameGrid) {
   let miniBoard = document.createElement('div');
   miniBoard.classList.add('mini-board');
-  renderMiniBoard(miniBoard, gameGrid)
+  renderMiniBoard(miniBoard, gameGrid);
   playerVictoryColumn.appendChild(miniBoard);
-}
+};
 
 function renderMiniBoard(miniBoard, gameGrid) {
   for (let i = 0; i < 3; i++) {
     let rows = gameGrid[i];
     renderMiniBoardRows(rows, miniBoard);
-  }
-}
+  };
+};
 
 function renderMiniBoardRows(rows, miniBoard) {
   for (let i = 0; i < 3; i++) {
     let miniToken = document.createElement('div');
     miniToken.innerText = rows[i];
     miniBoard.appendChild(miniToken);
-  }
-}
-
-function updatePlayerWinCount(player, winningPlayerStats) {
-  let winCount = winningPlayerStats.querySelector('.player-info h5')
-  winCount.innerText = `${player.wins.length} wins!`
-}
-
-function displayPlayerTurn() {
-  if (player1.turn === true) {
-    turnDisplay.innerHTML = `<h3>It's ${player1.name}'s turn!</h3>`
-  } else if (player2.turn === true) {
-    turnDisplay.innerHTML = `<h3>It's ${player2.name}'s turn!</h3>`
-  } else {
-    turnDisplay.innerHTML = `<h3>It's ${player1.name}'s turn!</h3>`
-  }
+  };
 };
 
-function resetVictoryCount() {
-  localStorage.clear();
-  location.reload();
-}
+function updatePlayerWinCount(player, winningPlayerStats) {
+  let winCount = winningPlayerStats.querySelector('.player-info h5');
+  winCount.innerText = `${player.wins.length} wins!`;
+};
 
+function displayPlayerTurn() {
+  (player1.turn) ? turnDisplay.innerHTML = `<h3>It's ${player1.name}'s turn!</h3>`
+  : (player2.turn) ? turnDisplay.innerHTML = `<h3>It's ${player2.name}'s turn!</h3>`
+  : turnDisplay.innerHTML = `<h3>It's ${player1.name}'s turn!</h3>`;
+};
 
 function resetGameBoard () {
   if (gameBoard.hadVictory || gameBoard.hadDraw) {
     let gameField = document.querySelector('.game-board');
     for (let i = 0; i < gameField.children.length; i++) {
       gameField.children[i].innerText = "";
-    }
+    };
     gameBoard.endGame();
-  }
-} 
+  };
+};
+
+function resetVictoryCount() {
+  localStorage.clear();
+  location.reload();
+};
+
